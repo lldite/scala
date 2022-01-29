e662876e4d78eef0216de1c14157420f0ff16fa8
lldite <lldite@gmail.com>
2022-01-29 17:24:58 +0200

    rombiņi

--- src/main/scala/Listi.scala
+++ src/main/scala/Listi.scala
@@ -2,10 +2,10 @@ object Listi {
   def main(): Unit = {
     println("Listi")
 
-    val l = List(1,2)++List(3,4)
-    println(l.map(x => (x*41).toString.reverse))
+    val l = List(1, 2) ++ List(3, 4)
+    println(l.map(x => (x * 41).toString.reverse))
 
-    println((1,2,3,4,5).toString.reverse == ")5,4,3,2,1(")
+    println((1, 2, 3, 4, 5).toString.reverse == ")5,4,3,2,1(")
 
     println((1 to 20).toList.filter(x => x % 2 == 0).map(x => (x, x + 1)))
 
@@ -13,5 +13,18 @@ object Listi {
 
     println(List.fill(10)("vīrietis").mkString(" ♥ sieviete ♥ "))
     // vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis ♥ sieviete ♥ vīrietis
+
+    // -----------------------------------
+
+    val augstums = 9 // trijstuurim
+    val kolonna = 7
+    val staavs = ((1 to augstums) ++ (augstums - 1 to 1 by -1)).toList
+
+    println(
+      List.fill(4)(staavs).flatten.map { i =>
+        // (List.fill(augstums - i)("-") var pārrakstīt uz "-" * (augstums - i) u.t.t.
+        (List.fill(augstums - i)("-") ++ List.fill(2 * i - 1)("*") ++ List.fill(augstums - i)("-")).mkString * kolonna
+      }.mkString("\n")
+    )
   }
 }
