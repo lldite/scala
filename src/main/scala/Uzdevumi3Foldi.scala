f5cac7259440ee2da1992cb4d1f21ec3c9020b8f
lldite <lldite@gmail.com>
2022-02-26 15:09:40 +0200

    atrisinati uzdevumi

--- src/main/scala/Uzdevumi3Foldi.scala
+++ src/main/scala/Uzdevumi3Foldi.scala
@@ -6,24 +6,35 @@ object Uzdevumi3Foldi {
   // n.foldLeft(0)((a, b) => a + b)  ==  (((0 + 1) + 2) + 3)
   // List(3,5,6,8) -> 22
   // n.foldLeft(0)((a, b) => a + b)  ==  ((((0 + 3) + 5) + 6) + 8)
-  def sumNumbers(n: List[Int]): Int = 0
+  def sumNumbers(ns: List[Int]): Int =
+    ns.foldLeft(0)((a, b) => a + b)
 
   // List() -> 1
   // List(1, 2, 3) -> 24
   // List(4, 9, 6) -> 350
-  def multiplyWithPlusOne(n: List[Int]): Int = 0
+  def multiplyWithPlusOne(ns: List[Int]): Int =
+    ns.foldLeft(1)((a, b) => a * (b + 1))
 
   // 123, List() -> 0
   // 749, List(107) -> 7
   // 210, List(5, 7, 2) -> 3
   // 420, List(3, 5, 7) -> 4
   // 1000, List(5, 2, 5, 2) -> 10
-  def divideBy(dividee: Int, divisors: List[Int]): Int = 0
+  def divideBy(dividee: Int, divisors: List[Int]): Int =
+    divisors.foldLeft(dividee)((a, b) => a / b)
 
   // List() -> ""
   // List("one", "two", three") -> " one two three"
   // List("1", "234", "5") -> " 1 234 5"
-  def sumStrings(strings: List[String]): String = ""
+  // HINT: mkString nevar lietot
+  def sumStrings(strings: List[String]): String =
+    strings.foldLeft("")((a, b) => a ++ (" " ++ b))
 
-  def count(a: List[Int]): Int = 0
+  // List() -> 0
+  // List(1, 2) -> 2
+  // List(5, 4, 1, 9) -> 4
+  // List(6) -> 1
+  // List(1, 5, 2, 0, -1) -> 5
+  def count(a: List[Int]): Int =
+    a.foldLeft(0)((a, b) => a + 1)
 }
